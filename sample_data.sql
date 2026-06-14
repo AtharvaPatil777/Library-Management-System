dba8af1b8b780ff8a92f45e47444360dfdaffd33
AtharvaPatil777 <112722462+AtharvaPatil777@users.noreply.github.com>
2026-06-14 16:30:25 +0530

    Create sample_data.sql

---
+++ sample_data.sql
@@ -0,0 +1,15 @@
+-- Insert sample data
+INSERT INTO books VALUES
+(1, 'Java Basics', 'James Gosling', 5),
+(2, 'SQL Guide', 'Unknown', 5),
+(3, 'DSA', 'Mark Allen', 4);
+
+INSERT INTO students VALUES
+(101, 'Ruchi', 'CS'),
+(102, 'Amit', 'IT'),
+(103, 'Neha', 'CS');
+
+INSERT INTO issue_records VALUES
+(1, '2026-04-01', NULL, 1, 101),
+(2, '2026-04-02', '2026-04-05', 2, 102),
+(3, '2026-04-03', '2026-04-03', 1, 103);
